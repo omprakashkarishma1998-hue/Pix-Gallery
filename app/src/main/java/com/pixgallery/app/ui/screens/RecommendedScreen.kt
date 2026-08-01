fb6033753cf12cc22202e0f9975e6baea282e2c7
omprakashkarishma1998-hue <omprakash.karishma1998@gmail.com>
2026-08-01 19:22:00 +0545

    Update RecommendedScreen.kt

--- app/src/main/java/com/pixgallery/app/ui/screens/RecommendedScreen.kt
+++ app/src/main/java/com/pixgallery/app/ui/screens/RecommendedScreen.kt
@@ -1,16 +1,7 @@
 package com.pixgallery.app.ui.screens
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.weight
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
